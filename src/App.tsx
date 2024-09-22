5a3dfd662985451440e611dbe7345b3643f24c67
Pol <polmonteroalegre@gmail.com>
2024-09-22 13:30:07 +0200

    FEAT: basic header, and gsap install

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
+import Navbar from "./components/Navbar"
+
 const App = () => {
 
 
   return (
     <>
       <main className="bg-black">
-        Navbar
-        Hero
-        Hightlights
+        <Navbar />
+        <Hero />
+        <Highlights />
       </main>
     </>
   )
