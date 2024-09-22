51085a25e11bdef40afa23ce0200958792501010
Pol <polmonteroalegre@gmail.com>
2024-09-22 12:52:48 +0200

    FEAT: add resources and provisional 3d models

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ const App = () => {
 
   return (
     <>
-      <h1 className="text-sky-500 font-bold underline">
-        Tailwind working
-      </h1>
+      <main className="bg-black">
+        Navbar
+        Hero
+        Hightlights
+      </main>
     </>
   )
 }
